fa32f5a0098baeeed4e8059c77aea8c7391b5859
tunogya <tunogya@qq.com>
2021-08-23 16:38:37 +0800

    fix: update test

--- test/test/wkdt.test.js
+++ test/test/wkdt.test.js
@@ -1,17 +1,14 @@
 import path from "path";
-import {emulator, getAccountAddress, init, shallPass, shallResolve, shallRevert} from "flow-js-testing";
+import {emulator, getAccountAddress, init, shallPass, shallResolve} from "flow-js-testing";
 import {deployWkdt, getWkdtBalance, getWkdtSupply, setupWkdtOnAccount, transferWkdt} from "../src/wkdt";
 import {getWakandaAdminAddress, toUFix64} from "../src/common";
 
-// Increase timeout if your tests failing due to timeout
 jest.setTimeout(10000);
 
 describe("wkdt", ()=>{
   beforeEach(async () => {
     const basePath = path.resolve(__dirname, "../../");
-		// You can specify different port to parallelize execution of describe blocks
-    const port = 8080; 
-		// Setting logging flag to true will pipe emulator output to console
+    const port = 8080;
     const logging = false;
     
     await init(basePath, { port, logging });
@@ -23,7 +20,7 @@ describe("wkdt", ()=>{
   });
 
   it("deploy wkdt", async () => {
-    await deployWkdt();
+    await shallPass(deployWkdt());
     const supply = await getWkdtSupply();
     expect(supply).toBe(toUFix64(7000000));
     const WakandaAdmin = await getWakandaAdminAddress();
